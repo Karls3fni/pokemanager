cf1277810c8f09a1a5ba2ecd7c242cfff616d6f0
Manu <m.viladorado@gmail.com>
2026-04-08 10:22:11 +0200

    add pokeservice (#1)
    * add every files
    
    * add test
    
    * deleted test/poke_service.py

--- test/test_pokeservice.py
+++
@@ -1,9 +0,0 @@
-from app.services.pokeapi import PokeService
-
-def test_get_pokemon_by_name():
-    service = PokeService()
-
-    pokemon = service.get_pokemon_by_name("pikachu")
-
-    assert pokemon is not None
-    assert pokemon.name == "pikachu"
